1d132839a6a8ec0ef985541c6f2cbdc3f5578d0b
sendyakbar <msendyakbars@gmail.com>
2018-10-31 19:27:39 +0700

    add winner feature

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function tictactoe() {
             }
             else if (xo[random] === 'O') {
                 counterO += 1
-                if(counterO < 5) {
+                if (counterO < 5) {
                     arr.push(xo[random])
                 }
                 else {
@@ -29,6 +29,63 @@ function tictactoe() {
         board.push(arr)
     }
     return board
-} 
+}
 
-console.log(tictactoe())
+function winner(game) {
+
+    let scoreX = 0
+    let scoreO = 0
+    for (let i = 0; i < game.length; i++) {
+        if (i === 0) {
+            // horizontal check
+            if(game[i][0] === 'X' && game[i][1] === 'X' && game[i][2] === 'X') scoreX += 1
+            if(game[i][0] === 'O' && game[i][1] === 'O' && game[i][2] === 'O') scoreO += 1
+
+            // vertical check
+            if(game[0][i] === 'X' && game[1][i] === 'X' && game[2][i] === 'X') scoreX += 1
+            if(game[0][i] === 'O' && game[1][i] === 'O' && game[2][i] === 'O') scoreO += 1
+
+            // diagonal check
+            if(game[i][0] === 'X' && game[i + 1][1] === 'X' && game[i + 2][2] === 'X') scoreX += 1
+            if(game[i][0] === 'O' && game[i + 1][1] === 'O' && game[i + 2][2] === 'O') scoreX += 1
+        }
+        else if (i === 2) {
+                // horizontal check
+                if(game[i][0] === 'X' && game[i][1] === 'X' && game[i][2] === 'X') scoreX += 1
+                if(game[i][0] === 'O' && game[i][1] === 'O' && game[i][2] === 'O') scoreO += 1
+    
+                // vertical check
+                if(game[0][i] === 'X' && game[1][i] === 'X' && game[2][i] === 'X') scoreX += 1
+                if(game[0][i] === 'O' && game[1][i] === 'O' && game[2][i] === 'O') scoreO += 1
+    
+                // diagonal check
+                if(game[i][0] === 'X' && game[i - 1][1] === 'X' && game[i - 2][2] === 'X') scoreX += 1
+                if(game[i][0] === 'O' && game[i - 1][1] === 'O' && game[i - 2][2] === 'O') scoreX += 1
+        }
+        else {
+            // horizontal check
+            if(game[i][0] === 'X' && game[i][1] === 'X' && game[i][2] === 'X') scoreX += 1
+            if(game[i][0] === 'O' && game[i][1] === 'O' && game[i][2] === 'O') scoreO += 1
+
+            // vertical check
+            if(game[0][i] === 'X' && game[1][i] === 'X' && game[2][i] === 'X') scoreX += 1
+            if(game[0][i] === 'O' && game[1][i] === 'O' && game[2][i] === 'O') scoreO += 1
+        }
+    }
+    console.log(`X vs O
+${scoreX} vs ${scoreO}`)
+    if (scoreX > scoreO) {
+        return 'X Win!'
+    }
+    else if (scoreX < scoreO) {
+        return 'O Win!'
+    }
+    else {
+        return 'Draw'
+    }
+}
+
+let array = tictactoe()
+console.log(array.join('\n'))
+console.log('')
+console.log(winner(array))
